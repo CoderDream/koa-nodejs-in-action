3258e0c80cde6dc93b6fbc61dc39a5e72a02273e
CoderDream <coderdream@gmail.com>
2019-06-05 10:47:48 +0800

    update

--- vedio-lesson/0-start/koa2-tutorial/app.js
+++ vedio-lesson/0-start/koa2-tutorial/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const Koa = require('koa')
+const app = new Koa()
+
+// 增加代码
+app.use(async (ctx, next) => {
+    await next()
+    ctx.response.type = 'text/html'
+    ctx.response.body = '<h1>Hello World</h1>'
+})
+
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000')
+})
