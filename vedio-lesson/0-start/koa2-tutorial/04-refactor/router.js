f816224b3a86bf13492b480e4b03b979f6e8e74d
CoderDream <coderdream@gmail.com>
2019-06-06 10:42:26 +0800

    分离 controller 层 20190606

--- vedio-lesson/0-start/koa2-tutorial/04-refactor/router.js
+++ vedio-lesson/0-start/koa2-tutorial/04-refactor/router.js
@@ -1,44 +1,23 @@
 const router = require('koa-router')()
+const HomeController = require('./controller/home')
 
 module.exports = (app) => {
-    router.get('/', async (ctx, next) => {
-        ctx.response.body = `<h1>index page</h1>`
-    })
-// 请求参数放在 URL 中间
-    router.get('/home/:id/:name', async (ctx, next) => {
-        console.log(ctx.params)
-        ctx.response.body = '<h1>HOME page ' + ctx.params.id + ' ' + ctx.params.name + '</h1>'
-    })
+    router.get('/', HomeController.index)
+
+    // 请求参数放在 URL 中间
+    router.get('/home/:id/:name', HomeController.home)
 
     router.get('/404', async (ctx, next) => {
         ctx.response.body = '<h1>404 Not Found</h1>'
     })
 
-// 增加返回表单页面的路由
-    router.get('/user', async (ctx, next) => {
-        ctx.response.body =
-            `
-      <form action="/user/register" method="post">
-        <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
-        <br/>
-        <input name="password" type="text" placeholder="请输入密码：123456"/>
-        <br/> 
-        <button>GoGoGo</button>
-      </form>
-    `
-    })
+    // 增加返回表单页面的路由
+    router.get('/user', HomeController.login)
 
-// 增加响应表单请求的路由
-    router.post('/user/register', async (ctx, next) => {
-        let {name, password} = ctx.request.body
-        if (name === 'ikcamp' && password === '123456') {
-            ctx.response.body = `Hello， ${name}！`
-        } else {
-            ctx.response.body = '账号信息错误'
-        }
-    })
+    // 增加响应表单请求的路由
+    router.post('/user/register', HomeController.register)
 
-// add router middleware:
+    // add router middleware:
     app.use(router.routes())
         .use(router.allowedMethods())
 }
