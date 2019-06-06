f98a13f3b2a4f35c7f384fbdad594678b97c9967
CoderDream <coderdream@gmail.com>
2019-06-06 10:59:14 +0800

    视图 Nunjucks 20190606

---
+++ vedio-lesson/0-start/koa2-tutorial/05-nunjucks/router.js
@@ -0,0 +1,23 @@
+const router = require('koa-router')()
+const HomeController = require('./controller/home')
+
+module.exports = (app) => {
+    router.get('/', HomeController.index)
+
+    // 请求参数放在 URL 中间
+    router.get('/home/:id/:name', HomeController.home)
+
+    router.get('/404', async (ctx, next) => {
+        ctx.response.body = '<h1>404 Not Found</h1>'
+    })
+
+    // 增加返回表单页面的路由
+    router.get('/user', HomeController.login)
+
+    // 增加响应表单请求的路由
+    router.post('/user/register', HomeController.register)
+
+    // add router middleware:
+    app.use(router.routes())
+        .use(router.allowedMethods())
+}
