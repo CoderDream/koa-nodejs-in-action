9716eaaeb4d832459dec913d256ad9c659c407a8
CoderDream <coderdream@gmail.com>
2019-06-09 11:27:01 +0800

    抽出可配置量 20190609

--- vedio-lesson/0-start/koa2-tutorial/08-mi-log/middleware/mi-log/logger.js
+++ vedio-lesson/0-start/koa2-tutorial/08-mi-log/middleware/mi-log/logger.js
@@ -1,22 +1,38 @@
 const log4js = require('log4js')
 const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
+// 提取默认公用参数对象
+const baseInfo = {
+    appLogLevel: 'debug',  // 指定记录的日志级别
+    dir: 'logs',		// 指定日志存放的目录名
+    env: 'dev',   // 指定当前环境，当为开发环境时，在控制台也输出，方便调试
+    projectName: 'koa2-tutorial',  // 项目名，记录在日志中的项目信息
+    serverIp: '0.0.0.0'		// 默认情况下服务器 ip 地址
+}
 
+const { env, appLogLevel, dir } = baseInfo
 module.exports = () => {
     const contextLogger = {}
 
-    const config = {
-        appenders: {
-            cheese: {
-                type: 'dateFile', // 日志类型
-                filename: `logs/task`,  // 输出的文件名
-                pattern: 'yyyy-MM-dd.log',  // 文件名增加后缀
-                alwaysIncludePattern: true   // 是否总是有后缀名
-            }
-        },
+    const appenders = {}
+
+    appenders.cheese = {
+        type: 'dateFile',
+        filename: `${dir}/task`,
+        pattern: '-yyyy-MM-dd.log',
+        alwaysIncludePattern: true
+    }
+    // 环境变量为dev local development 认为是开发环境
+    if (env === "dev" || env === "local" || env === "development") {
+        appenders.out = {
+            type: "console"
+        }
+    }
+    let config = {
+        appenders,
         categories: {
             default: {
-                appenders: ['cheese'],
-                level:'info'
+                appenders: Object.keys(appenders),
+                level: appLogLevel
             }
         }
     }
