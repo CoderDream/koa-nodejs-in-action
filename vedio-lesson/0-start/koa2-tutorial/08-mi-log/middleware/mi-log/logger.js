a576ca77223c2ab8bdb5d8a177e720051939fa04
CoderDream <coderdream@gmail.com>
2019-06-09 11:22:22 +0800

    实现应用日志，将其挂载到 ctx 上 20190609

--- vedio-lesson/0-start/koa2-tutorial/08-mi-log/middleware/mi-log/logger.js
+++ vedio-lesson/0-start/koa2-tutorial/08-mi-log/middleware/mi-log/logger.js
@@ -1,19 +1,43 @@
 const log4js = require('log4js')
+const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
 
 module.exports = () => {
-    return async (ctx, next) => {
-        const start = Date.now()
-        log4js.configure({
-            appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
-            categories: { default: { appenders: ['cheese'], level: 'info' } }
-        });
+    const contextLogger = {}
 
-        const logger = log4js.getLogger('cheese')
+    const config = {
+        appenders: {
+            cheese: {
+                type: 'dateFile', // 日志类型
+                filename: `logs/task`,  // 输出的文件名
+                pattern: 'yyyy-MM-dd.log',  // 文件名增加后缀
+                alwaysIncludePattern: true   // 是否总是有后缀名
+            }
+        },
+        categories: {
+            default: {
+                appenders: ['cheese'],
+                level:'info'
+            }
+        }
+    }
+
+    const logger = log4js.getLogger('cheese')
+
+    return async (ctx, next) => {
+        // 记录请求开始的时间
+        const start = Date.now()
+        log4js.configure(config)
+        // 循环methods将所有方法挂载到ctx 上
+        methods.forEach((method, i) => {
+            contextLogger[method] = (message) => {
+                logger[method](message)
+            }
+        })
+        ctx.log = contextLogger;
 
         await next()
-
-        const end = Date.now()
-        const responseTime = end - start;
+        // 记录完成的时间 作差 计算响应时间
+        const responseTime = Date.now() - start;
         logger.info(`响应时间为${responseTime/1000}s`);
     }
 }
