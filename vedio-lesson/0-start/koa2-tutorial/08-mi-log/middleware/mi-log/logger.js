2856d3b5d7b940aba17fa3d93a7d4e715336d348
CoderDream <coderdream@gmail.com>
2019-06-09 15:25:25 +0800

    丰富日志信息 20190606

--- vedio-lesson/0-start/koa2-tutorial/08-mi-log/middleware/mi-log/logger.js
+++ vedio-lesson/0-start/koa2-tutorial/08-mi-log/middleware/mi-log/logger.js
@@ -1,27 +1,29 @@
-const log4js = require('log4js')
+const log4js = require('log4js');
+// 引入日志输出信息的封装文件
+const access = require("./access.js");
 const methods = ["trace", "debug", "info", "warn", "error", "fatal", "mark"]
-// 提取默认公用参数对象
-const baseInfo = {
-    appLogLevel: 'debug',  // 指定记录的日志级别
-    dir: 'logs',		// 指定日志存放的目录名
-    env: 'dev',   // 指定当前环境，当为开发环境时，在控制台也输出，方便调试
-    projectName: 'koa2-tutorial',  // 项目名，记录在日志中的项目信息
-    serverIp: '0.0.0.0'		// 默认情况下服务器 ip 地址
-}
 
-const { env, appLogLevel, dir } = baseInfo
+const baseInfo = {
+    appLogLevel: 'debug',
+    dir: 'logs',
+    env: 'dev',
+    projectName: 'koa2-tutorial',
+    serverIp: '0.0.0.0'
+}
+const { env, appLogLevel, dir, serverIp, projectName } = baseInfo
+// 增加常量，用来存储公用的日志信息
+const commonInfo = { projectName, serverIp }
 module.exports = () => {
     const contextLogger = {}
-
     const appenders = {}
 
     appenders.cheese = {
         type: 'dateFile',
         filename: `${dir}/task`,
-        pattern: '-yyyy-MM-dd.log',
+        pattern: 'yyyy-MM-dd.log',
         alwaysIncludePattern: true
     }
-    // 环境变量为dev local development 认为是开发环境
+
     if (env === "dev" || env === "local" || env === "development") {
         appenders.out = {
             type: "console"
@@ -37,25 +39,24 @@ module.exports = () => {
         }
     }
 
-    const logger = log4js.getLogger('cheese')
+    const logger = log4js.getLogger('cheese');
 
     return async (ctx, next) => {
-        // 记录请求开始的时间
         const start = Date.now()
+
         log4js.configure(config)
-        // 循环methods将所有方法挂载到ctx 上
         methods.forEach((method, i) => {
             contextLogger[method] = (message) => {
-                logger[method](message)
+                // 将入参换为函数返回的字符串
+                logger[method](access(ctx, message, commonInfo))
             }
         })
         ctx.log = contextLogger;
 
         await next()
-        // 记录完成的时间 作差 计算响应时间
         const responseTime = Date.now() - start;
-        logger.info(`响应时间为${responseTime/1000}s`);
+        logger.info(access(ctx, {
+            responseTime: `响应时间为${responseTime/1000}s`
+        }, commonInfo))
     }
 }
-
-
